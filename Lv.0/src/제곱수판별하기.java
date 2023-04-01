887a1cd004ebcf20cb086d04d251032b871fdc08
nayoon <nayoonk928@gmail.com>
2023-04-02 01:37:16 +0900

    제곱수판별하기

---
+++ Lv.0/src/제곱수판별하기.java
@@ -0,0 +1,11 @@
+public class 제곱수판별하기 {
+    public int solution(int n) {
+
+        if (Math.sqrt(n) % 1 == 0) {
+            return 1;
+        } else {
+            return 2;
+        }
+
+    }
+}
