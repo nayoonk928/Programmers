cd5c710ce43d7e2a435be393c0df63e6bc2de6ed
nayoon <nayoonk928@gmail.com>
2023-04-02 19:55:02 +0900

    k번째

---
+++ Lv.1/src/k번째.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class k번째 {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+
+        for (int i = 0; i < commands.length; i++) {
+            int startIdx = commands[i][0] - 1;
+            int endIdx = commands[i][1] - 1;
+            int find = commands[i][2];
+
+            PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+            for (int j = startIdx; j <= endIdx; j++) {
+                pq.offer(array[j]);
+            }
+
+            // find - 1 번째 까지 poll
+            for (int k = 0; k < find - 1; k++) {
+                pq.poll();
+            }
+
+            // find 번째 요소 poll하며 answer에 넣기
+            answer[i] = pq.poll();
+        }
+        return answer;
+    }
+}
