d5ebe3bb922055c92842f4956db8d09b0350424d
nayoon <nayoonk928@gmail.com>
2023-04-28 01:07:15 +0900

    Lv. 0 컨트롤 제트 upload

---
+++ Lv.0/src/컨트롤_제트.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(String s) {
+        String[] str = s.split(" ");
+        int answer = Integer.parseInt(str[0]);
+
+        for (int i = 1; i < str.length; i++) {
+            if (str[i].equals("Z")) {
+                answer -= Integer.parseInt(str[i - 1]);
+            } else {
+                answer += Integer.parseInt(str[i]);
+            }
+        }
+
+        return answer;
+    }
+}
